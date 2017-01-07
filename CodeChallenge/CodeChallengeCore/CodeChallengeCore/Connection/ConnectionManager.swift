fd3243f04885adfffab16a67fe8c178147787f84
Jose Torres <torrescardenasjose@gmail.com>
2017-01-07 13:56:52 -0500

    Setup internal core framework dependency manager with Carthage.

---
+++ CodeChallenge/CodeChallengeCore/CodeChallengeCore/Connection/ConnectionManager.swift
@@ -0,0 +1,15 @@
+//
+//  ConnectionManager.swift
+//  CodeChallengeCore
+//
+//  Created by Jose Torres on 7/1/17.
+//  Copyright © 2017 Jose Torres. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct Connection {
+    
+    
+}
