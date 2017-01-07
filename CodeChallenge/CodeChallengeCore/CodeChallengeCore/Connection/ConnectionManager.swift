b405a9344e8807ad653bd8b4dfadc7cf6c330a0b
Jose Torres <torrescardenasjose@gmail.com>
2017-01-07 14:43:11 -0500

    Base implementation for ConnectionManager using Alamofire.

--- CodeChallenge/CodeChallengeCore/CodeChallengeCore/Connection/ConnectionManager.swift
+++ CodeChallenge/CodeChallengeCore/CodeChallengeCore/Connection/ConnectionManager.swift
@@ -9,7 +9,36 @@
 import Foundation
 import Alamofire
 
-struct Connection {
+public enum Result<T> {
     
+    case Failure(Error)
+    case Success(T)
+}
+
+enum ConnectionError: Error {
     
+    case responseNotJSON
+    case unkwnon
+}
+
+public struct Connection {
+    
+    public init() {
+        
+    }
+    
+    fileprivate func requestJSON(url: String, completion: @escaping (Result<AnyObject>) -> Void) {
+        
+        Alamofire.request(url).responseJSON { response in
+            
+            dump(response)
+            
+            guard let responseJSON = response.result.value else {
+                
+                return completion(.Failure(ConnectionError.responseNotJSON))
+            }
+            
+            completion(.Success(responseJSON as AnyObject))
+        }
+    }
 }
