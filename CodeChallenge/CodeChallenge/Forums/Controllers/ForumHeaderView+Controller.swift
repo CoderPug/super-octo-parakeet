ce119211ca7019ed31af616153218a59d72718d8
Jose Torres <torrescardenasjose@gmail.com>
2017-01-08 01:40:35 -0500

    Added image loading for ForumHeaderView.

--- CodeChallenge/CodeChallenge/Forums/Controllers/ForumHeaderView+Controller.swift
+++ CodeChallenge/CodeChallenge/Forums/Controllers/ForumHeaderView+Controller.swift
@@ -8,13 +8,27 @@
 
 import Foundation
 import CodeChallengeCore
+import PromiseKit
 
 extension ForumHeaderView {
     
     //  MARK: Public
     
-    func load(forum: Forum) {
+    func load(_ forum: Forum) {
         
         labelTitle.text = forum.title
+        if let url = forum.headerImageURL {
+            
+            imageBackground.getPromiseImage(url: url)
+                .then { [weak self] _ in
+                    
+                    self?.progressView.stopAnimating()
+                    
+                }.catch { error in
+                    
+                    dump(error)
+            }
+        }
     }
+    
 }
